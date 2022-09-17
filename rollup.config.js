e8d1ac7068b198634f9860783095d7d06b72c1c5
Piotr Pliszczyński <fookoo@gmail.com>
2022-09-17 23:03:22 +0200

    Feature/introduce automation (#4)
    Add ESM module

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
+const common = {
+  exports: 'auto',
+  preserveModules: true,
+  preserveModulesRoot: 'src',
+  sourcemap: false
+}
+
 export default [
   {
-    external: [
-      '@emotion/react',
-      '@emotion/styled',
-      '@mui/material',
-      'react',
-      'react-dom',
-      'rxjs',
-      'uuid',
-      'tslib'
-    ],
     input: ['src/index.ts', 'src/hooks/index.ts', 'src/components/index.ts', 'src/styled/index.ts'],
     output: [
       {
-        dir: 'dist',
+        dir: 'dist/cjs',
         format: 'cjs',
-        exports: 'auto',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        sourcemap: true
+        ...common
+      },
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        ...common
       }
     ],
     plugins: [
