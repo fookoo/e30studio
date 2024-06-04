f70e7e34f0dd1a7a15b575e62d6a3f15443cfe39
Piotr Pliszczyński <fookoo@gmail.com>
2024-06-04 12:38:53 +0200

    Cleanup (#9)
    Change to yarn
    ---------
    
    Co-authored-by: Piotr Pliszczyński <piotr.pilczynski@frontegg.com>

--- scripts/prepare-package.js
+++ scripts/prepare-package.js
@@ -1,12 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJSON = require('./../package.json')
+
 const cb = (err) => {
   if (err) throw err
 }
-
+// eslint-disable-next-line no-console
+console.log('Copying README.md ...')
 fs.copyFile('README.md', './dist/README.md', cb)
 delete packageJSON.devDependencies
 delete packageJSON.scripts
+// eslint-disable-next-line no-console
+console.log('Writing package.json ...')
 fs.writeFile('./dist/package.json', JSON.stringify(packageJSON, null, '\t'), (err) => {
   if (err) {
     console.error(err)
