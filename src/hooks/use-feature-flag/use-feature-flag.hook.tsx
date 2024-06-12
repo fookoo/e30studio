5dd43c46e5e53eaef98484b479a7163f0dc6c360
Piotr Pliszczyński <fookoo@gmail.com>
2024-06-12 10:04:15 +0200

    useFeatureFlag support both vite and CRA

--- src/hooks/use-feature-flag/use-feature-flag.hook.tsx
+++ src/hooks/use-feature-flag/use-feature-flag.hook.tsx
@@ -1,10 +1,24 @@
 import { useMemo } from 'react'
 
+type ViteMeta = {
+  env: {
+    [key: string]: string
+  }
+}
+
+const getEnv = (name: string) => {
+  if (typeof import.meta !== 'undefined' && 'env' in import.meta) {
+    const viteEnvs = import.meta as unknown as ViteMeta
+
+    return viteEnvs.env[`VITE_${name.toUpperCase()}`]
+  } else if (typeof process !== 'undefined' && process.env) {
+    return process.env[`REACT_APP_${name}`]
+  } else {
+    console.warn(`Environment variable ${name} is not defined`)
+
+    return undefined
+  }
+}
+
 export const useFeatureFlag = <T extends string>(flags: T[]): boolean[] =>
-  useMemo(
-    () =>
-      flags
-        .map((flag) => `REACT_APP_${flag.toUpperCase()}`)
-        .map((flag) => (process.env[flag] || '').toLowerCase() === 'true'),
-    [flags]
-  )
+  useMemo(() => flags.map((flag) => String(getEnv(flag)).toUpperCase() === 'TRUE'), [flags])
