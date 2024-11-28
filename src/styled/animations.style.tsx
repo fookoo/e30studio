74259af672218fe6a78da82bd1659315e3cbc90f
Piotr Pliszczyński <fookoo@gmail.com>
2024-11-28 11:55:30 +0100

    Feature/0.0.38 (#22)
    * Add Animations to export
    
    * bump version

--- src/styled/animations.style.tsx
+++ src/styled/animations.style.tsx
@@ -17,3 +17,21 @@ export const SlideFromRight = css`
   animation-iteration-count: 1;
   animation-fill-mode: forwards;
 `
+
+export const SlideFromLeft = css`
+  @keyframes slideFromLeft {
+    0% {
+      transform: translateX(-130%);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  transform: translateX(-130%);
+
+  animation-name: slideFromLeft;
+  animation-duration: 350ms;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+`
