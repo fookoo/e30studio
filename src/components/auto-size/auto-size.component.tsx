2c459515cc7b3a37f9f944c65235503d768da0c2
Piotr Pliszczyński <fookoo@gmail.com>
2025-06-25 17:28:51 +0200

    fix prettier

--- src/components/auto-size/auto-size.component.tsx
+++ src/components/auto-size/auto-size.component.tsx
@@ -52,7 +52,7 @@ export const AutoSize: React.FC<IAutoSizerProps> = ({ style, children }) => {
       observer = new ResizeObserver(debounced)
       observer.observe(observedElement)
     } else {
-      // this is a fallback if ResizeObserver is not supported
+      // this is fallback if ResizeObserver is not supported
       window.addEventListener('resize', debounced)
 
       setTimeout(debounced, 100)
