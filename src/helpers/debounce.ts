44ca7fd2e8517954d85274238f8dc696f84a4e01
Piotr Pliszczyński <fookoo@gmail.com>
2025-10-03 10:21:17 +0200

    Refactor Autosize

--- src/helpers/debounce.ts
+++
@@ -1,8 +0,0 @@
-export const debounce = function debounce<T extends Function>(cb: T, wait = 20) {
-  let h: NodeJS.Timeout
-
-  return (...args: unknown[]) => {
-    clearTimeout(h)
-    h = setTimeout(() => cb(...args), wait)
-  }
-}
